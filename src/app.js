8eeae4634b68591446a32d35057bc41b0237447e
Kima93 <154838127+Kima93@users.noreply.github.com>
2025-02-20 19:37:38 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com'];
 
+function generateDomains(pronoun, adj, noun, extension) {
+    let domains = [];
+    for (let p of pronoun) {
+        for (let a of adj) {
+            for (let n of noun) {
+                for (let ext of extension) {
+                    domains.push(`${p}${a}${n}${ext}`);
+                }
+            }
+        }
+    }
+    return domains;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(generateDomains(pronoun, adj, noun, domainExtensions));
